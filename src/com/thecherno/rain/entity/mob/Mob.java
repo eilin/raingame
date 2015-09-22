abbb99e0ab083dd48d4800b751569e397f4b0681
eilin <edward.id.lin@gmail.com>
2015-09-21 23:46:17 -0400

    end of ep 42 - set up framework for mobs

---
+++ src/com/thecherno/rain/entity/mob/Mob.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package com.thecherno.rain.entity.mob;
+
+import com.thecherno.rain.entity.*;
+import com.thecherno.rain.graphics.*;
+/**
+ * @author Edward
+ *
+ */
+public abstract class Mob extends Entity
+{
+	protected Sprite sprite;
+	protected int direction = 0; //north = 0, east = 1, south = 2, west = 3
+	protected boolean moving = false;
+	
+	public boolean isMoving() {
+		return moving;
+	}
+	
+	/**
+	 * args describe the change in position of the mob
+	 */
+	public void move(int delta_x, int delta_y) {
+
+		if (delta_x > 0) {
+			direction = 1;
+		} else {
+			direction = 3;
+		}
+		
+		if (delta_y > 0) {
+			direction = 0;
+		} else {
+			direction = 4;
+		}
+		
+		if (!isCollision()) {
+			x += delta_x;
+			y += delta_y;
+		}
+	}
+	
+	public void update() {
+		//todo
+	}
+	
+	private boolean isCollision() {
+		return false; //temp default for now
+	}
+}
